02b066b8b027625464e80bc7bc5de9c5c0b9c01f
Andreas Lindhé <andreas@lindhe.io>
2020-08-06 18:17:28 +0200

    Reindent file

--- checkfiles.py
+++ checkfiles.py
@@ -39,51 +39,51 @@ fix = {
     }
 
 def main(path, verbose = False):
-  """ Takes a path and checks the filenames for issues. """
-  # This stolen from here: https://stackoverflow.com/a/3207973/893211
-  original_filenames = []
-  for (_, _, filenames) in os.walk(path):
-    original_filenames.extend(filenames)
-    break
-  bad_filenames = findProblems(strings = original_filenames)
-  fixes = findFixes(bad_filenames)
-  check_for_collisions(original_filenames, fixes.values())
-  if verbose:
-    print("Original filenames: " + str(original_filenames))
-    print("Bad filenames: " + str(bad_filenames))
-    print("Fixed filenames: " + str(fixes.values()))
-  for fix in fixes:
-    print("mv " + fix + " " + fixes[fix])
+    """ Takes a path and checks the filenames for issues. """
+    # This stolen from here: https://stackoverflow.com/a/3207973/893211
+    original_filenames = []
+    for (_, _, filenames) in os.walk(path):
+        original_filenames.extend(filenames)
+        break
+    bad_filenames = findProblems(strings = original_filenames)
+    fixes = findFixes(bad_filenames)
+    check_for_collisions(original_filenames, fixes.values())
+    if verbose:
+        print("Original filenames: " + str(original_filenames))
+        print("Bad filenames: " + str(bad_filenames))
+        print("Fixed filenames: " + str(fixes.values()))
+    for fix in fixes:
+        print("mv " + fix + " " + fixes[fix])
 
 
 def findFixes(broken_strings: List[str]) -> Dict[str,str]:
-  fixes = {}
-  for string in broken_strings:
-    fixes[string] = fixProblem(string)
-  return fixes
+    fixes = {}
+    for string in broken_strings:
+        fixes[string] = fixProblem(string)
+    return fixes
 
 def check_for_collisions(a: List[str], b: List[str], warning = "WARNING! Collision found!"):
-  """ If elements in lists a and b are similar, print a warning and exit """
-  collision = set(a) & set(b)
-  if collision:
-    sys.exit(warning + "\n\n" + str(collision))
+    """ If elements in lists a and b are similar, print a warning and exit """
+    collision = set(a) & set(b)
+    if collision:
+        sys.exit(warning + "\n\n" + str(collision))
 
 def findProblems(strings: List[str]) -> List[str]:
-  bad_strings: List[str] = []
-  for shit in fix:
-    bad_strings = bad_strings + [s for s in strings if shit in s]
-  return bad_strings
+    bad_strings: List[str] = []
+    for shit in fix:
+        bad_strings = bad_strings + [s for s in strings if shit in s]
+    return bad_strings
 
 def fixProblem(bad_string: str) -> str:
-  """ Takes a bad string and fixes it """
-  for shit in fix:
-      bad_string = re.sub(pattern=shit, repl=fix[shit], string=bad_string)
-  return bad_string
+    """ Takes a bad string and fixes it """
+    for shit in fix:
+        bad_string = re.sub(pattern=shit, repl=fix[shit], string=bad_string)
+    return bad_string
 
 
 if __name__ == '__main__':
-  args = docopt(docs, version=__version__)
-  try:
-    main(path = args['PATH'], verbose = args['--verbose'])
-  except KeyboardInterrupt:
-    sys.exit("\nInterrupted by ^C\n")
+    args = docopt(docs, version=__version__)
+    try:
+        main(path = args['PATH'], verbose = args['--verbose'])
+    except KeyboardInterrupt:
+        sys.exit("\nInterrupted by ^C\n")
